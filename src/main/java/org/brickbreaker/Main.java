86f92828d3bc051d38dfe8c1a9af99b6552a1c4e
Kev <93066593+Dr-Hackerstein@users.noreply.github.com>
2024-04-23 12:24:12 -0600

    Finished the brick breaker game

--- src/main/java/org/brickbreaker/Main.java
+++ src/main/java/org/brickbreaker/Main.java
@@ -4,16 +4,13 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        JFrame frame = new JFrame();
+        JFrame obj = new JFrame();
         Gameplay gamePlay = new Gameplay();
-
-        frame.setBounds(10,10,700,600);
-        frame.setTitle("Breakout Ball");
-        frame.setResizable(false);
-        frame.setVisible(true);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.add(gamePlay);
-
-
+        obj.setBounds(10, 10, 700, 600);
+        obj.setTitle("Breakout Ball");
+        obj.setResizable(false);
+        obj.setVisible(true);
+        obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        obj.add(gamePlay);
     }
 }
