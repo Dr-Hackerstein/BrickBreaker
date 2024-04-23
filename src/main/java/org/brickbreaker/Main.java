7614ab57b58838403e6f99cc75c3fb0dd12aebee
Kev <93066593+Dr-Hackerstein@users.noreply.github.com>
2024-04-23 11:13:46 -0600

    Created the Main class and the Gameplay class. In the main class I worked on the JFrame and initialize the Gameplay class

--- src/main/java/org/brickbreaker/Main.java
+++ src/main/java/org/brickbreaker/Main.java
@@ -1,7 +1,19 @@
 package org.brickbreaker;
 
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        JFrame frame = new JFrame();
+        Gameplay gamePlay = new Gameplay();
+
+        frame.setBounds(10,10,700,600);
+        frame.setTitle("Breakout Ball");
+        frame.setResizable(false);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(gamePlay);
+
+
     }
 }
